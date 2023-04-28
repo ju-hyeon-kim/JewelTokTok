50acda2b1aff573c6da7c8a0be003b9a7bdc8ff5
ju-hyeon-kim <63406146+ju-hyeon-kim@users.noreply.github.com>
2023-04-28 18:35:22 +0900

    0428
    JH_logic

--- Assets/Scripts/PlayScene/PlayManager.cs
+++ Assets/Scripts/PlayScene/PlayManager.cs
@@ -7,6 +7,7 @@ public class PlayManager : MonoBehaviour
 {
     public Transform Cubes;
     public GameObject Cube;
+    GameObject[,] CubeArray = new GameObject[8,8];
 
     void Start()
     {
@@ -20,7 +21,6 @@ public class PlayManager : MonoBehaviour
         int height = 8;
         //간격
         float Interval = 0.625f;
-
         for (int w = 0; w < width; w++)
         {
             for(int h  = 0; h < height; h++)
@@ -29,7 +29,15 @@ public class PlayManager : MonoBehaviour
                 cube.transform.position += new Vector3(w * Interval, h * Interval, 0);
                 //랜덤 색 입히기
                 RandomColorPaint(cube.GetComponent<SpriteRenderer>());
+                //배열에 저장
+                CubeArray[w, h] = cube;
             }
+        }
+
+        //매치3가 있는지 검사 -> 있다면 색깔 바꿔주기
+        if(isMatch3Exist(CubeArray))
+        {
+            Debug.Log("매치3가 존재합니다.");
         }
     }
 
@@ -54,5 +62,37 @@ public class PlayManager : MonoBehaviour
                 cube.color = Color.cyan;
                 break;
         }
+    }
+
+    bool isMatch3Exist(GameObject[,] cubearray) // 매치3가 존재하는지 판단하는 함수
+    {
+        for (int w = 0; w < cubearray.GetLength(0); w++)
+        {
+            for (int h = 0; h < cubearray.GetLength(1); h++)
+            {
+                //각 큐브가 매치3에 해당하는지 검사
+
+                //자신의 가로 줄에 동일한 색상큐브 2개가 더 있는지 검사
+                if(cubearray[w, h].GetComponent<SpriteRenderer>().color == cubearray[w+1, h].GetComponent<SpriteRenderer>().color)
+                {
+                    //현재 큐브와 오른쪽 1칸 옆의 큐브와 색이 같다.
+                }
+                if (cubearray[w, h].GetComponent<SpriteRenderer>().color == cubearray[w + 2, h].GetComponent<SpriteRenderer>().color)
+                {
+                    //현재 큐브와 오른쪽 2칸 옆의 큐브와 색이 같다.
+                }
+                if (cubearray[w, h].GetComponent<SpriteRenderer>().color == cubearray[w - 1, h].GetComponent<SpriteRenderer>().color)
+                {
+                    //현재 큐브와 왼쪽 1칸 옆의 큐브와 색이 같다.
+                }
+                if (cubearray[w, h].GetComponent<SpriteRenderer>().color == cubearray[w - 2, h].GetComponent<SpriteRenderer>().color)
+                {
+                    //현재 큐브와 왼쪽 2칸 옆의 큐브와 색이 같다.
+                }
+
+                //자신의 세로 줄에 동일한 색상큐브 2개가 더 있는지 검사
+            }
+        }
+        return true;
     }
 }
