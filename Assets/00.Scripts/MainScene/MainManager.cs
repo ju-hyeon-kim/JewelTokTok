56bd72a06300647044bd42d2eca06c13d58dbf71
ju-hyeon-kim <63406146+ju-hyeon-kim@users.noreply.github.com>
2023-04-29 01:24:29 +0900

    (JH)UI_Work
    1

--- Assets/00.Scripts/MainScene/MainManager.cs
+++ Assets/00.Scripts/MainScene/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//ÁÖÇö
 public class MainManager : MonoBehaviour
 {
     public void StartButton() // OnClick
--- Assets/00.Scripts/MainScene/TitleScreen.cs
+++ Assets/00.Scripts/MainScene/TitleScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//ÁÖÇö
 public class TitleScreen : MonoBehaviour
 {
     public GameObject MakeNicknameWindow;
