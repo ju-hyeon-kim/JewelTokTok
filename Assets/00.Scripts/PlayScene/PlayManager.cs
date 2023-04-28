35b4c76d90da5754ecf6406e5597f847598646f9
qkrdudwns000 <114732265+qkrdudwns000@users.noreply.github.com>
2023-04-28 22:11:38 +0900

    (PYJ) 드로우시 3match 예외처리

--- Assets/00.Scripts/PlayScene/PlayManager.cs
+++ Assets/00.Scripts/PlayScene/PlayManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayManager : MonoBehaviour
 {
-    public Transform Cubes;
-    public GameObject Cube;
+    // 필요 컴퍼넌트
+    public Transform Cubes; // block 오브젝트 모아둘 부모 트랜스폼.
+    public GameObject Cube; // block 프리팹.
+    Block[,] blocklist = null; // 모든 block들 list
+    public List<Sprite> characters = new List<Sprite>();
+    // 보드 크기 가로x세로 (8x8)
+    [SerializeField] int width = 8;
+    [SerializeField] int height = 8;
 
     void Start()
     {
@@ -15,44 +20,34 @@ public class PlayManager : MonoBehaviour
 
     void MakeBoard() // 보드 만들기 -> 예외 처리: 처음 만들어진 보드판에는 '빙고!'가 있어서는 안됨
     {
-        //가로x세로 8x8
-        int width = 8;
-        int height = 8;
+        blocklist = new Block[width, height];
         //간격
         float Interval = 0.625f;
+
+        Sprite[] previousLeft = new Sprite[height];
+        Sprite previousBelow = null;
 
         for (int w = 0; w < width; w++)
         {
             for(int h  = 0; h < height; h++)
             {
+                
                 GameObject cube = Instantiate(Cube, Cubes);
                 cube.transform.position += new Vector3(w * Interval, h * Interval, 0);
-                //랜덤 색 입히기
-                RandomColorPaint(cube.GetComponent<SpriteRenderer>());
-            }
-        }
-    }
+                blocklist[w, h] = cube.GetComponent<Block>();
 
-    void RandomColorPaint(SpriteRenderer cube)
-    {
-        int rnd = Random.Range(0, 4);
-        switch(rnd)
-        {
-            case 0:
-                cube.color = Color.red;
-                break;
-            case 1:
-                cube.color = Color.blue;
-                break;
-            case 2:
-                cube.color = Color.green;
-                break;
-            case 3:
-                cube.color = Color.yellow;
-                break;
-            case 4:
-                cube.color = Color.cyan;
-                break;
+                List<Sprite> possibleCharacters = new List<Sprite>();
+                possibleCharacters.AddRange(characters);
+                possibleCharacters.Remove(previousLeft[h]);
+                possibleCharacters.Remove(previousBelow);
+
+                // 랜덤 sprite 입히기
+                Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+                cube.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                previousLeft[h] = newSprite;
+                previousBelow = newSprite;
+            }
         }
     }
 }
